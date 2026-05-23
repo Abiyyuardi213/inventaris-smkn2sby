249edde33a6a0297074653f95e6ae3171469bf00
Maulana-Dra <maulana.dra06@gmail.com>
2026-05-23 14:57:27 +0700

    feat: routes & sidebar

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,10 @@
 
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\JurusanController;
+use App\Http\Controllers\KategoriController;
 use App\Http\Controllers\RoleController;
+use App\Http\Controllers\RuanganController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
@@ -15,4 +18,7 @@ Route::middleware('auth')->group(function () {
 
     Route::resource('roles', RoleController::class);
     Route::resource('users', UserController::class);
+    Route::resource('jurusans', JurusanController::class);
+    Route::resource('ruangans', RuanganController::class);
+    Route::resource('kategoris', KategoriController::class);
 });
