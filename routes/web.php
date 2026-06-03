c94a19a31acd5fbd75d2be997279235c4522f65e
Maulana-Dra <maulana.dra06@gmail.com>
2026-06-03 12:57:53 +0700

    chore: Add admin prefix to routes on maulana branch

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ Route::get('/', [AuthController::class, 'login'])->name('login');
 Route::post('/', [AuthController::class, 'authenticate'])->name('login.authenticate');
 Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
 
-Route::middleware('auth')->group(function () {
+Route::middleware('auth')->prefix('admin')->group(function () {
     Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
 
     Route::resource('roles', RoleController::class);
